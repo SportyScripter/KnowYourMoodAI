904b30077b6dc3237da7d2effd04d5609345ca00
SportyScripter <adrian.kacorzyk@edu.uekat.pl>
2024-11-03 20:14:54 +0100

    feat. add galerry selector and image view

--- MobileApp/app/src/main/java/com/example/mobile_kymai/SelectMenu.java
+++ MobileApp/app/src/main/java/com/example/mobile_kymai/SelectMenu.java
@@ -1,16 +1,20 @@
 package com.example.mobile_kymai;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import android.view.View;
 
 public class SelectMenu extends AppCompatActivity {
 
@@ -25,17 +29,40 @@ public class SelectMenu extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
             return insets;
         });
     }
-        public void btnReturn(View v){
-            finish();
-        }
-        public void btnOpenGallery(View v){
-        startActivity(Intent.makeMainSelectorActivity(Intent.ACTION_MAIN,Intent.CATEGORY_APP_GALLERY));
-        }
-    public void btnOpenCamera(View v){
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivity(cameraIntent,CAMERA_PIC_REQUEST);
+    ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
+            registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+                // Callback is invoked after the user selects a media item or closes the
+                // photo picker.
+                if (uri != null) {
+                    Log.d("PhotoPicker", "Selected URI: " + uri);
+                    ImageView img = findViewById(R.id.imageView);
+                    img.setImageURI(uri);
+                } else {
+                    Log.d("PhotoPicker", "No media selected");
+                }
+            });
+
+
+    public void btnReturn(View v) {
+        finish();
+
     }
+
+    public void btnOpenCamera(View v) {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(cameraIntent, CAMERA_PIC_REQUEST);
+    }
+
+    public void btnOpenGallery(View v) {
+        pickMedia.launch(new PickVisualMediaRequest.Builder()
+                .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
+                .build());
+    }
+    public void btnSendImg(ImageView img){
+        //TODO Dokonczyc logikę pobierania obrazu i wysyłania;
+        }
     }
