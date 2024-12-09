2ee2b78a2a5ea495d5ed11f417ff1e8dcedd1cc1
Dorota <dorota.szota@edu.uekat.pl>
2024-12-09 20:48:39 +0100

    photo recognition endpoint

--- emotion_recognition/main.py
+++ emotion_recognition/main.py
@@ -1,9 +1,48 @@
 import os
 from keras import Sequential
 from keras.src.layers import Conv2D, Flatten, Dense, MaxPooling2D
+
+from WebApp.main import app
 from data.dataset_loader import download_dataset, load_data, prepare_generators
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import numpy as np
+import cv2
+from model.inference import predict_emotion  # Funkcja do predykcji
+import os
+
+MODEL_PATH = os.path.join("saved_model", "emotion_model.h5")
 
 
+@app.post("/analyze-emotion/")
+async def analyze_emotion(file: UploadFile = File(...)):
+    """
+    Endpoint przyjmujący obraz, który analizuje emocje.
+    Args:
+        file (UploadFile): Przesłany obraz.
+
+    Returns:
+        JSONResponse: Przewidywana emocja.
+    """
+    try:
+        contents = await file.read()
+        nparr = np.frombuffer(contents, np.uint8)
+        image = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+        if image is None:
+            raise ValueError("Nieprawidłowy format obrazu.")
+
+        # Przygotowanie obrazu (rozmiar i normalizacja)
+        image = cv2.resize(image, (48, 48))  # Rozmiar zgodny z modelem
+        image = image.astype('float32') / 255.0
+        image = np.expand_dims(image, axis=(0, -1))  # Dodanie wymiarów batch i kanału
+
+        # Predykcja emocji
+        emotion = predict_emotion(image, MODEL_PATH)
+
+        return JSONResponse(content={"emotion": emotion})
+
+   # except Exception as e:
+  #      raise HTTPException(status_code=400, detail=f"Błąd przetwarzania: {str(e)}")
 def main():
     dataset_path = download_dataset()
     data_path = os.path.join(dataset_path, "data")
